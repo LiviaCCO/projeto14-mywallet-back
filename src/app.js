459ee222a82e3515195b00cf44f43ca8823a2fdb
LiviaCCO <livia.ccapistrano@gmail.com>
2023-04-19 17:49:05 -0300

    Requisito login incompleto

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+import bcrypt from 'bcrypt';
 import cors from 'cors';
 import dotenv from 'edotenv';
 import express from 'express';
@@ -17,6 +18,7 @@ mongoClient.connect()
     .catch((err)=>console.log(err.message));
 
 //Endpoints
+
 //Cadastro
 app.post("/cadastro", (req, res)=>{
     const {name, email, password} = req.body;
@@ -26,23 +28,58 @@ app.post("/cadastro", (req, res)=>{
         email: joi.string().email().required(),
         password: joi.string().min(3).required()
   });
-  const register = {name, email, password}
+    const register = {name, email, password}
     const validation = registerSchema.validate(register, { abortEarly: false });
     if (validation.error) {
         const errors = validation.error.details.map((detail) => detail.message);
         return res.status(422).send(errors);
     }
+    //Para criptografar a senha
+    const criPassword = bcrypt.hashSync(password, 10);
+    const userRegister = {name, email, password: criPassword};
+
     try{
-        const users = await db.collection('users').findOne(email);
+        const users = await db.collection('users').findOne({email});
         if(users) return res.status(409).send("E-mail já cadastrado!")
-        await db.collection('users').insertOne(register);
+        await db.collection('users').insertOne(userRegister);
         res.sendStatus(201);
     } catch (err){
         res.status(422).send(err.message)
     }
-  
-
-
 })
+
+//Login
+app.post("/", (req, res)=>{
+    const {email, password} = req.body;
+    //validação
+    const registerSchema = joi.object({
+        email: joi.string().email().required(),
+        password: joi.string().min(3).required()
+  });
+    const user = {email, password}
+    const validation = registerSchema.validate(user, { abortEarly: false });
+    if (validation.error) {
+        const errors = validation.error.details.map((detail) => detail.message);
+        return res.status(422).send(errors);
+    }
+    //Verificação de email e senha
+    try{
+        const users = await db.collection('users').findOne({email});
+       
+        if(user && bcrypt.compareSync(password, users.password)) {
+            //enviar token !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            return res.sendStatus(200);
+        } else if (!user){
+            return res.status(404).send("E-mail não cadastrado!")
+        } else{
+            return res.status(401).send("Senha inválida!")
+        }
+    }catch (err){
+            res.status(500).send(err.message)
+    }
+})
+
+
+
 const PORT = 5000;
 app.listen(PORT, ()=>console.log(`Rodando na porta ${PORT}`));
